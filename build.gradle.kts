30ecf4918e896fbc534c033e0d9be3e83ecb7be8
André Roaldseth <andre.roaldseth@nav.no>
2023-12-19 16:02:08 +0100

    Oppgrader til nyeste R&R med ny snappy

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2023080113411690890096.310ed8e5ed93")
+    implementation("com.github.navikt:rapids-and-rivers:2023120410321701682379.55cc1a24d803")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
 
     implementation(kotlin("test"))
